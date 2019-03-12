30ed4667c79105619182818c80854e8588db42f4
Manoj <48181339+nightcrawler99@users.noreply.github.com>
2019-03-12 20:26:16 +0000

    Play list challenge
    My attempt to solve play list challenge
    -create album
    -create play list of songs which are in albums
    features
    -menu/list all songs/play songs/go back to previous song/replay song/delete song

---
+++ Songs.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Songs {
+
+    private String name;
+    private String duration;
+
+
+    public Songs(String name, String duration) {
+        this.name = name;
+        this.duration = duration;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+
+}
